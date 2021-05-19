1dea53912e39afdf0f67a9ed169547b86923df38
Dimitar.Petroww <Mitko66692@abv.bg>
2021-05-19 13:53:13 +0300

    update

--- src/controllers/postBreed.js
+++ src/controllers/postBreed.js
@@ -1,6 +1,9 @@
 const breeds = require("../../util/breeds.json")
+const formidable = require("formidable")
+const Formidable = require("formidable")
 
 function postBreed(req, res) {
-    
+    const form = new Formidable
+
 }
 module.exports = postBreed
